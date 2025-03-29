dad31f43f0dcc1fc4fe681d156a801ce8489661c
Rocky Pukar <ckrp2018@gmail.com>
2025-03-29 19:22:53 +0545

    feat(test): edit hotel playwright tests

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -75,3 +75,21 @@ test("should display hotels", async ({ page }) => {
   ).toBeVisible();
   await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
 });
+
+test("should edit hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}hotels`);
+
+  await page.getByRole("link", { name: "View Details" }).first().click();
+
+  await page.waitForSelector("[name='name']", { state: "attached" });
+  await expect(page.locator("[name='name']")).toHaveValue("<name>");
+  await page.locator("[name='name']").fill("<name> updated");
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+
+  await page.reload();
+
+  await expect(page.locator("[name='name']")).toHaveValue("<name> UPDATED");
+  await page.locator("[name='name']").fill("<name>");
+  await page.getByRole("button", { name: "Save" }).click();
+});
